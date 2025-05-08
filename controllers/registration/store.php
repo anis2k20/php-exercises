f4be45cf554380f50ffaa13292cb996824dc7755
Anis <anis2k20@gmail.com>
2025-05-08 08:30:26 +0600

    feat: add password hash

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -34,7 +34,7 @@ if($user){
 }else{
     $db->query('INSERT INTO users(email,password) VALUE(:email, :password)',[
         'email' => $email,
-        'password' => $password,
+        'password' => password_hash($password, PASSWORD_BCRYPT),
     ]);
 
     $_SESSION['user'] = [

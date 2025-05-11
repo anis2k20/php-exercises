d9a3a193ec80f13b159b229915acf14e3a7e1d0c
Anis <anis2k20@gmail.com>
2025-05-11 08:23:02 +0600

    feat: add authenticator class

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,46 +1,22 @@
 <?php
 
-use core\App;
-use core\Database;
-use core\validator;
+use core\Authenticator;
 use Http\Forms\LoginForm;
 
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-if(! $form->validate($email, $password)){
-    return view('sessions/create.view.php', [
-                'errors' => $form->errors(),
-            ]);
-}
-
-
-// match
-$user = $db->query('select * from users where email = :email',[
-    'email'=>$email
-])->find();
-
-if($user){
-    if(password_verify($password, $user['password'])){
-
-        login([
-            'email'=> $email
-        ]);
-        header('location: /');
-        exit();
+if ($form->validate($email, $password)) {
+    if ((new Authenticator())->attempt($email, $password)) {
+        redirect('/');
     }
-    // return view('sessions/create.view.php',[
-    //     'errors' => [
-    //         'email'=>'No matching account found according to this email.'
-    //     ],
-    // ]);
+    $form->error('email', 'No matching email found');
 }
 
-return view('session/create.view.php',[
-    'errors' => [
-        'email'=>'No matching account found according to this email and password.'
-    ],
+
+return view('sessions/create.view.php', [
+    'errors' => $form->errors(),
 ]);
+
--- core/functions.php
+++ core/functions.php
@@ -44,21 +44,9 @@ function view($path, $attributes)
     require base_path('views/' . $path);
 }
 
-function login($user)
-{
-    $_SESSION['user'] = [
-        'emial' => $user['email'],
-    ];
-    
-    session_regenerate_id(true);
+function redirect($path){
+    header("location: {$path}");
+    exit();
 }
 
-function logout()
-{
-    $_SESSION = [];
-    session_destroy();
 
-    $params = session_get_cookie_params();
-
-    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-}
